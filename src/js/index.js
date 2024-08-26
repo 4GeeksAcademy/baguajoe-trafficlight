16646ec54eaf64f5b30955362b10fd0ea041f514
baguajoe <baguajoe@gmail.com>
2024-08-26 01:24:54 +0000

    traffic2

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,9 @@ import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
 
 
+import TrafficLight from "../component.app.js
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.createRoot(document.getElementById('app')).render(<TrafficLight/>);
 
